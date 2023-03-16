44d530cdad742cfdb209a26ad8dcc3859c411c81
neaj <neaj@appscode.com>
2023-03-16 19:15:00 +0600

    C8:main map

---
+++ map.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+
+	fmt.Println(elements["Li"])
+}
