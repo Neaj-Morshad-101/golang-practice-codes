31856b59c5cf4b31b5516a290deebca2e0baacae
neaj <neaj@appscode.com>
2023-03-16 19:25:52 +0600

    C9:main map

--- map.go
+++ map.go
@@ -3,17 +3,50 @@ package main
 import "fmt"
 
 func main() {
-	elements := make(map[string]string)
-	elements["H"] = "Hydrogen"
-	elements["He"] = "Helium"
-	elements["Li"] = "Lithium"
-	elements["Be"] = "Beryllium"
-	elements["B"] = "Boron"
-	elements["C"] = "Carbon"
-	elements["N"] = "Nitrogen"
-	elements["O"] = "Oxygen"
-	elements["F"] = "Fluorine"
-	elements["Ne"] = "Neon"
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name":  "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name":  "Carbon",
+			"state": "solid",
+		},
+		"N": map[string]string{
+			"name":  "Nitrogen",
+			"state": "gas",
+		},
+		"O": map[string]string{
+			"name":  "Oxygen",
+			"state": "gas",
+		},
+		"F": map[string]string{
+			"name":  "Fluorine",
+			"state": "gas",
+		},
+		"Ne": map[string]string{
+			"name":  "Neon",
+			"state": "gas",
+		},
+	}
 
-	fmt.Println(elements["Li"])
+	if el, ok := elements["Li"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
 }
