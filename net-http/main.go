c9ee05644bf083709af0fbeeb84e5d31f88a0982
neaj <neaj@appscode.com>
2023-03-21 18:46:40 +0600

    C25:main simple server

--- net-http/main.go
+++ net-http/main.go
@@ -5,17 +5,44 @@ import (
 	"net/http"
 )
 
+// Handler interface
 type welcome string
 
 func (wc welcome) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to our server!")
 }
 
+// Handler function
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Login Page!")
+}
+
+// Handler function
+func logout(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Logout Page!")
+}
+
+func getJson
+
 func main() {
+
+	//Router
+	router := http.NewServeMux()
+
+	//Handler
 	var wc welcome
+
+	router.Handle("/", wc)
+
+	//Handler Funcs
+	router.HandleFunc("/logout", logout)
+
+	router.HandleFunc("/login", login)
+
+	//server
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: wc,
+		Handler: router,
 	}
 
 	//run the server
