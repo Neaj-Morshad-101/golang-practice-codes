5c917be385a8b0b3fa795d490d5390810b11e6b8
neaj <neaj@appscode.com>
2023-03-22 11:38:25 +0600

    C:last commit from main simple api server

--- net-http/main.go
+++ net-http/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
+
+// User Struct
+type User struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
 
 // Handler interface
 type welcome string
@@ -22,7 +30,33 @@ func logout(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Logout Page!")
 }
 
-func getJson
+func getJson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	switch r.Method {
+	case "GET":
+		w.Write([]byte(`"message":"GET is called!"`))
+	case "POST":
+		w.Write([]byte(`"message":"POST is called!"`))
+	}
+}
+
+func checkUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	dbPassword := "neaj101"
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		log.Fatal("error decoding into struct")
+	}
+
+	if user.Password == dbPassword {
+		fmt.Println("Success Logged In")
+	}
+
+	fmt.Fprintf(w, "Response: %v", user)
+}
 
 func main() {
 
@@ -38,6 +72,10 @@ func main() {
 	router.HandleFunc("/logout", logout)
 
 	router.HandleFunc("/login", login)
+
+	router.HandleFunc("/json", getJson)
+
+	router.HandleFunc("/user", checkUser)
 
 	//server
 	server := http.Server{
