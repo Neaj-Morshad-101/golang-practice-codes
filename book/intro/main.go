4d5db2f8ba83ce1db23961403ffd4f3df700a9b9
Neaj-Morshad-101 <neajmorshad.ashik101@gmail.com>
2023-07-21 23:20:49 +0600

    added-all
    Signed-off-by: Neaj-Morshad-101 <neajmorshad.ashik101@gmail.com>

---
+++ book/intro/main.go
@@ -0,0 +1,50 @@
+package main
+
+// this is a comment
+
+func main() {
+	//fmt.Println("Hello World")
+	//fmt.Println("1 + 1 =", 1+1)
+	//fmt.Println("1 + 1 =", 1.0+1.0)
+	//fmt.Println(len("Hello World"))
+	//fmt.Println("Hello World"[1])               // 101 instead of e
+	//fmt.Printf("char : %v\n", "Hello World"[1]) // 101 instead of e
+	//fmt.Println("Hello " + "World")
+
+	//var x string
+	//x = "first "
+	//fmt.Println(x)
+	//x = x + "second"
+	//fmt.Println(x)
+
+	//fmt.Print("Enter a number: ")
+	//var input float64
+	//fmt.Scanf("%f", &input)
+	//
+	//output := input * 2
+	//
+	//fmt.Println(output)
+
+	//for i := 1; i <= 10; i++ {
+	//	if i%2 == 0 {
+	//		fmt.Println(i, "even")
+	//	} else {
+	//		fmt.Println(i, "odd")
+	//	}
+	//
+	//}
+
+	//var arr [3]int
+	//arr[1] = 100
+	//fmt.Println(arr)
+
+	//slice1 := []int{1, 2, 3}
+	//slice2 := append(slice1, 4, 5)
+	//fmt.Println(slice1, slice2)
+
+	//slice1 := []int{1, 2, 3}
+	//slice2 := make([]int, 2)
+	//copy(slice2, slice1)
+	//fmt.Println(slice1, slice2)
+
+}
---
+++ book/intro/readme.md
@@ -0,0 +1,3 @@
+An Introduction to Programming in Go
+https://www.golang-book.com/books/intro
+
