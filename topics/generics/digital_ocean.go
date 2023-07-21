be75d9830aa97207dbc85c0fcabbe0053262cb6d
neaj <neaj@appscode.com>
2023-07-21 17:08:10 +0600

    added-all
    Signed-off-by: neaj <neaj@appscode.com>

--- topics/generics/digital_ocean.go
+++ topics/generics/digital_ocean.go
@@ -1,3 +1,3 @@
-https://www.digitalocean.com/community/tutorials/how-to-use-generics-in-go
+// https://www.digitalocean.com/community/tutorials/how-to-use-generics-in-go
+// learn and exercise the codes from above link:
 package main
-
